58a1675b080201fdb350a1abaf6309cec95f2b8d
Kalaiyarasi Ganeshalingam <ashakalai31@gmail.com>
2021-10-14 16:07:25 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE IF NOT EXISTS petdb;
+
+USE petdb;
+
+CREATE TABLE IF NOT EXISTS pet (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name  VARCHAR(300),
+  category  VARCHAR(300),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('John', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Tommy', 'Dog', 1000);
+INSERT INTO petdb.pet VALUES ('Harry', 'Dog', 2000);
+INSERT INTO petdb.pet VALUES ('Benny', 'Cat', 1000);
+INSERT INTO petdb.pet VALUES ('Tigger', 'Cat', 2000);
+INSERT INTO petdb.pet VALUES ('Emma', 'Cat', 1000);
